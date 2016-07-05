265c36c93f485965212d64c9afa6fcf17d7bb830
Shaik Apsar <sa709c@intl.att.com>
2016-07-05 21:01:58 +0000

    [WIP] Trove Fuel Plugin update for OpenStack Fuel liberty release.
    Implements: blueprint fuel-plugin-for-trove-liberty
    
    Change-Id: Iddc7c6c867e10459d6aa9185528aedac29ae73b6

--- deployment_scripts/puppet/modules/trove/manifests/guestagent.pp
+++ deployment_scripts/puppet/modules/trove/manifests/guestagent.pp
@@ -54,6 +54,38 @@
 #   (optional) Control exchange.
 #   Defaults to 'trove'.
 #
+# TODO(shaikapsar): remove this once bug/1585783 merged to stable/liberty.
+# [*rabbit_hosts*]
+#   (optional) List of clustered rabbit servers.
+#   Defaults to the value set in the trove class.
+#   The default can generally be left unless the
+#   guests need to talk to the rabbit cluster via
+#   different IPs.
+#
+# TODO(shaikapsar): remove this once bug/1585783 merged to stable/liberty.
+# [*rabbit_host*]
+#   (optional) Location of rabbitmq installation.
+#   Defaults to the value set in the trove class.
+#   The default can generally be left unless the
+#   guests need to talk to the rabbit cluster via
+#   a different IP.
+#
+# TODO(shaikapsar): remove this once bug/1585783 merged to stable/liberty.
+# [*rabbit_port*]
+#   (optional) Port for rabbitmq instance.
+#   Defaults to the value set in the trove class.
+#   The default can generally be left unless the
+#   guests need to talk to the rabbit cluster via
+#   a different port.
+#
+# TODO(shaikapsar): remove this once bug/1585783 merged to stable/liberty.
+# [*rabbit_use_ssl*]
+#   (optional) Connect over SSL for RabbitMQ
+#   Defaults to the value set in the trove class.
+#   The default can generally be left unless the
+#   guests need to talk to the rabbit cluster via
+#   a different ssl connection option.
+#
 class trove::guestagent(
   $enabled                   = true,
   $manage_service            = true,
@@ -66,57 +98,91 @@ class trove::guestagent(
   $log_facility              = 'LOG_USER',
   $auth_url                  = 'http://localhost:5000/v2.0',
   $swift_url                 = 'http://localhost:8080/v1/AUTH_',
-  $control_exchange          = 'trove'
+  $control_exchange          = 'trove',
+  # TODO(shaikapsar): remove the below 4lines once bug/1585783 merged to stable/liberty.
+  $rabbit_hosts              = $::trove::rabbit_hosts,
+  $rabbit_host               = $::trove::rabbit_host,
+  $rabbit_port               = $::trove::rabbit_port,
+  $rabbit_use_ssl            = $::trove::rabbit_use_ssl,
 ) inherits trove {
 
   include ::trove::params
 
   Trove_guestagent_config<||> ~> Exec['post-trove_config']
   Trove_guestagent_config<||> ~> Service['trove-guestagent']
-  # Trove db sync is broken in Ubuntu packaging
-  # This is a temporary fix until it's fixed in packaging.
-  # https://bugs.launchpad.net/ubuntu/+source/openstack-trove/+bug/1451134
-  file { '/etc/trove/trove-guestagent.conf':
-    require => File['/etc/trove'],
-  }
-  File['/etc/trove/trove-guestagent.conf'] -> Trove_guestagent_config<||>
-  Trove_guestagent_config<||> -> Package[$::trove::params::guestagent_package_name]
 
   # basic service config
   trove_guestagent_config {
     'DEFAULT/verbose':                      value => $verbose;
     'DEFAULT/debug':                        value => $debug;
-    'DEFAULT/trove_auth_url':               value => $auth_url;
-    'DEFAULT/swift_url':                    value => $swift_url;
-    'DEFAULT/nova_proxy_admin_user':        value => $::trove::nova_proxy_admin_user;
-    'DEFAULT/nova_proxy_admin_tenant_name': value => $::trove::nova_proxy_admin_tenant_name;
-    'DEFAULT/nova_proxy_admin_pass':        value => $::trove::nova_proxy_admin_pass;
     'DEFAULT/control_exchange':             value => $control_exchange;
+    'DEFAULT/rpc_backend':                  value => $::trove::rpc_backend;
   }
 
-  if $::trove::rpc_backend == 'trove.openstack.common.rpc.impl_kombu' {
-      if ! $::trove::rabbit_password {
+  # (shaikapsar): Option to fetch auth_url and swift_url from the keystone catalog.
+
+  # auth_url
+  if $auth_url {
+    trove_guestagent_config { 'DEFAULT/trove_auth_url': value => $auth_url }
+  }
+  else {
+    trove_guestagent_config { 'DEFAULT/trove_auth_url': ensure => absent }
+  }
+
+  # swift_url
+  if $swift_url {
+    trove_guestagent_config { 'DEFAULT/swift_url': value => $swift_url }
+  }
+  else {
+    trove_guestagent_config { 'DEFAULT/swift_url': ensure => absent }
+  }
+
+  # region name
+  if $::trove::os_region_name {
+    trove_guestagent_config { 'DEFAULT/os_region_name': value => $::trove::os_region_name }
+  }
+  else {
+    trove_guestagent_config {'DEFAULT/os_region_name': ensure => absent }
+  }
+
+  if $::trove::rpc_backend == 'trove.openstack.common.rpc.impl_kombu' or $::trove::rpc_backend == 'rabbit' {
+    if ! $::trove::rabbit_password {
       fail('When rpc_backend is rabbitmq, you must set rabbit password')
     }
-    if $::trove::rabbit_hosts {
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_hosts':     value  => join($::trove::rabbit_hosts, ',') }
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_ha_queues': value  => true }
+
+    # TODO(shaikapsar): remove the below lines once bug/1585783 merged to stable/liberty.
+    if $rabbit_hosts {
+      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_hosts':     value  => join($rabbit_hosts, ',') }
     } else  {
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_host':      value => $::trove::rabbit_host }
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_port':      value => $::trove::rabbit_port }
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_hosts':     value => "${::trove::rabbit_host}:${::trove::rabbit_port}" }
-      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_ha_queues': value => false }
+      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_host':      value => $rabbit_host }
+      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_port':      value => $rabbit_port }
+      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_hosts':     value => "${rabbit_host}:${rabbit_port}" }
+    }
+
+    if $::trove::rabbit_ha_queues == undef {
+      # TODO(shaikapsar): remove the below lines once bug/1585783 merged to stable/liberty.
+      if size($rabbit_hosts) > 1 {
+        trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_ha_queues': value  => true }
+      } else {
+        trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_ha_queues': value => false }
+      }
+    } else {
+      trove_guestagent_config { 'oslo_messaging_rabbit/rabbit_ha_queues': value => $::trove::rabbit_ha_queues }
     }
 
     trove_guestagent_config {
-      'oslo_messaging_rabbit/rabbit_userid':         value => $::trove::rabbit_user;
+      'oslo_messaging_rabbit/rabbit_userid':         value => $::trove::rabbit_userid;
       'oslo_messaging_rabbit/rabbit_password':       value => $::trove::rabbit_password, secret => true;
       'oslo_messaging_rabbit/rabbit_virtual_host':   value => $::trove::rabbit_virtual_host;
-      'oslo_messaging_rabbit/rabbit_use_ssl':        value => $::trove::rabbit_use_ssl;
+      # TODO(shaikapsar): remove the below lines once bug/1585783 merged to stable/liberty.
+      'oslo_messaging_rabbit/rabbit_use_ssl':        value => $rabbit_use_ssl;
       'oslo_messaging_rabbit/kombu_reconnect_delay': value => $::trove::kombu_reconnect_delay;
+      # TODO(shaikapsar): remove the below lines once bug/1486319 merged to stable/liberty.
+      'oslo_messaging_rabbit/amqp_durable_queues':   value => $::trove::amqp_durable_queues;
     }
 
-    if $::trove::rabbit_use_ssl {
+    # TODO(shaikapsar): remove the below lines once bug/1585783 merged to stable/liberty.
+    if $rabbit_use_ssl {
 
       if $::trove::kombu_ssl_ca_certs {
         trove_guestagent_config { 'oslo_messaging_rabbit/kombu_ssl_ca_certs': value => $::trove::kombu_ssl_ca_certs; }
@@ -152,19 +218,22 @@ class trove::guestagent(
     }
   }
 
-  if $::trove::rpc_backend == 'trove.openstack.common.rpc.impl_qpid' {
+  if $::trove::rpc_backend == 'trove.openstack.common.rpc.impl_qpid' or $::trove::rpc_backend == 'qpid'{
+
+    warning('Qpid driver is removed from Oslo.messaging in the Mitaka release')
+
     trove_guestagent_config {
-      'DEFAULT/qpid_hostname':               value => $::trove::qpid_hostname;
-      'DEFAULT/qpid_port':                   value => $::trove::qpid_port;
-      'DEFAULT/qpid_username':               value => $::trove::qpid_username;
-      'DEFAULT/qpid_password':               value => $::trove::qpid_password, secret => true;
-      'DEFAULT/qpid_heartbeat':              value => $::trove::qpid_heartbeat;
-      'DEFAULT/qpid_protocol':               value => $::trove::qpid_protocol;
-      'DEFAULT/qpid_tcp_nodelay':            value => $::trove::qpid_tcp_nodelay;
+      'oslo_messaging_qpid/qpid_hostname':    value => $::trove::qpid_hostname;
+      'oslo_messaging_qpid/qpid_port':        value => $::trove::qpid_port;
+      'oslo_messaging_qpid/qpid_username':    value => $::trove::qpid_username;
+      'oslo_messaging_qpid/qpid_password':    value => $::trove::qpid_password, secret => true;
+      'oslo_messaging_qpid/qpid_heartbeat':   value => $::trove::qpid_heartbeat;
+      'oslo_messaging_qpid/qpid_protocol':    value => $::trove::qpid_protocol;
+      'oslo_messaging_qpid/qpid_tcp_nodelay': value => $::trove::qpid_tcp_nodelay;
     }
     if is_array($::trove::qpid_sasl_mechanisms) {
       trove_guestagent_config {
-        'DEFAULT/qpid_sasl_mechanisms': value => join($::trove::qpid_sasl_mechanisms, ' ');
+        'oslo_messaging_qpid/qpid_sasl_mechanisms': value => join($::trove::qpid_sasl_mechanisms, ' ');
       }
     }
   }
