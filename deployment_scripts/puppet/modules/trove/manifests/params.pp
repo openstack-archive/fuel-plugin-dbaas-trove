19ecb12372883933af3df59ce154b12e299be2dd
Shaik Apsar <sa709c@intl.att.com>
2016-07-11 20:28:09 +0000

    Trove Fuel Plugin update for OpenStack Fuel 8.0 release.
    Implements: blueprint fuel-plugin-for-trove-liberty
    
    Change-Id: Iddc7c6c867e10459d6aa9185528aedac29ae73b6

--- deployment_scripts/puppet/modules/trove/manifests/params.pp
+++ deployment_scripts/puppet/modules/trove/manifests/params.pp
@@ -14,6 +14,8 @@ class trove::params {
       $guestagent_service_name  = 'openstack-trove-guestagent'
       $taskmanager_package_name = 'openstack-trove-taskmanager'
       $taskmanager_service_name = 'openstack-trove-taskmanager'
+      $sqlite_package_name      = undef
+      $pymysql_package_name     = undef
     }
     'Debian': {
       $client_package_name      = 'python-troveclient'
@@ -26,6 +28,8 @@ class trove::params {
       $guestagent_service_name  = 'trove-guestagent'
       $taskmanager_package_name = 'trove-taskmanager'
       $taskmanager_service_name = 'trove-taskmanager'
+      $sqlite_package_name      = 'python-pysqlite2'
+      $pymysql_package_name     = 'python-pymysql'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem")
