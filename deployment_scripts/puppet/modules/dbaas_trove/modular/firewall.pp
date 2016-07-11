59c8d8195f36f1b3bf6a27e01871fc87fa8e9ed7
Shaik Apsar <sa709c@intl.att.com>
2016-07-11 20:28:09 +0000

    Trove Fuel Plugin update for OpenStack Fuel 8.0 release.
    Implements: blueprint fuel-plugin-for-trove-liberty
    
    Change-Id: Iddc7c6c867e10459d6aa9185528aedac29ae73b6

---
+++ deployment_scripts/puppet/modules/dbaas_trove/modular/firewall.pp
@@ -0,0 +1,1 @@
+include ::dbaas_trove::firewall
--- deployment_scripts/puppet/modules/trove/spec/acceptance/basic_trove_spec.rb
+++ deployment_scripts/puppet/modules/trove/spec/acceptance/basic_trove_spec.rb
@@ -6,41 +6,11 @@ describe 'basic trove' do
 
     it 'should work with no errors' do
       pp= <<-EOS
-      Exec { logoutput => 'on_failure' }
-
-      # Common resources
-      case $::osfamily {
-        'Debian': {
-          include ::apt
-          class { '::openstack_extras::repo::debian::ubuntu':
-            release         => 'kilo',
-            package_require => true,
-          }
-          $package_provider = 'apt'
-        }
-        'RedHat': {
-          class { '::openstack_extras::repo::redhat::redhat':
-            release => 'kilo',
-          }
-          package { 'openstack-selinux': ensure => 'latest' }
-          $package_provider = 'yum'
-        }
-        default: {
-          fail("Unsupported osfamily (${::osfamily})")
-        }
-      }
-
-      class { '::mysql::server': }
-
-      class { '::rabbitmq':
-        delete_guest_user => true,
-        package_provider  => $package_provider,
-      }
-
-      rabbitmq_vhost { '/':
-        provider => 'rabbitmqctl',
-        require  => Class['rabbitmq'],
-      }
+      include ::openstack_integration
+      include ::openstack_integration::repos
+      include ::openstack_integration::rabbitmq
+      include ::openstack_integration::mysql
+      include ::openstack_integration::keystone
 
       rabbitmq_user { 'trove':
         admin    => true,
@@ -57,29 +27,9 @@ describe 'basic trove' do
         require              => Class['rabbitmq'],
       }
 
-      # Keystone resources, needed by Trove to run
-      class { '::keystone::db::mysql':
-        password => 'keystone',
-      }
-      class { '::keystone':
-        verbose             => true,
-        debug               => true,
-        database_connection => 'mysql://keystone:keystone@127.0.0.1/keystone',
-        admin_token         => 'admin_token',
-        enabled             => true,
-      }
-      class { '::keystone::roles::admin':
-        email    => 'test@example.tld',
-        password => 'a_big_secret',
-      }
-      class { '::keystone::endpoint':
-        public_url => "https://${::fqdn}:5000/",
-        admin_url  => "https://${::fqdn}:35357/",
-      }
-
       # Trove resources
       class { '::trove':
-        database_connection   => 'mysql://trove:a_big_secret@127.0.0.1/trove?charset=utf8',
+        database_connection   => 'mysql+pymysql://trove:a_big_secret@127.0.0.1/trove?charset=utf8',
         rabbit_userid         => 'trove',
         rabbit_password       => 'an_even_bigger_secret',
         rabbit_host           => '127.0.0.1',
@@ -94,10 +44,19 @@ describe 'basic trove' do
       class { '::trove::api':
         keystone_password => 'a_big_secret',
         auth_url          => 'http://127.0.0.1:35357/',
+        debug             => true,
+        verbose           => true,
       }
       class { '::trove::client': }
-      class { '::trove::conductor': }
-      class { '::trove::taskmanager': }
+      class { '::trove::conductor':
+        debug   => true,
+        verbose => true,
+      }
+      class { '::trove::taskmanager':
+        debug   => true,
+        verbose => true,
+      }
+      class { '::trove::quota': }
       EOS
 
 
